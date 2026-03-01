4981369659d8cee86c5e92376f6885aa17da0ef2
Kaushal Kishor <kkup.06009@gmail.com>
2026-03-01 22:11:17 +0530

    new logical error code

--- Chapter 8 errors and debugg/app.js
+++ Chapter 8 errors and debugg/app.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const testingSyntax = require('./syntax')
+const logical = require('./logical')
 
 const server = http.createServer((req,res)=>{
   console.log(req.url,req.method);
   testingSyntax();
+  logical();
 })
 
 const PORT = 3001;
