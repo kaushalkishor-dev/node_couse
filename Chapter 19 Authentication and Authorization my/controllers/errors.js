dc79220e6545cbf9008a5daa353f490bebb9d215
Kaushal Kishor <kkup.06009@gmail.com>
2026-03-31 01:12:33 +0530

    Delete Chapter 19 Authentication and Authorization my directory

--- Chapter 19 Authentication and Authorization my/controllers/errors.js
+++
@@ -1,9 +0,0 @@
-exports.pageNotFound = (req, res, next) => {
-  res
-    .status(404)
-    .render("404", {
-      pageTitle: "Page Not Found",
-      currentPage: "Error",
-      isLoggedIn: req.session.isLoggedIn,
-    });
-};
